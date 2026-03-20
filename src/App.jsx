2824bb46d1917b85197fb61bdf080050df385e7c
saloni  Maurya <manshimaurya195@gmail.com>
2026-03-20 14:27:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import ProductList from "./pages/Productlist.jsx";
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
+import Footer from "./components/footer.jsx";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ToastContainer, toast,Bounce } from "react-toastify";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Bounce}
+        />
+        <div className="min-h-screen bg-gray-950 font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
+  );
+};
+
+export default App;
